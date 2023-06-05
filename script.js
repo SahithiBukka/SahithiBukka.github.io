314d1bef3427521db099a6ae47660691cd6efc4c
SahithiBukka <93440705+SahithiBukka@users.noreply.github.com>
2023-06-05 21:24:24 +0530

    changes made

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-alert("Pop up");
+var a={image:"Pic.jpeg",name:"Sahithi B.G",designation:"Student"};
+var a={image:"Pic1.jpeg",name:"Santhu",designation:"Software Developer"};
+var isa=true;
+var changeMypic=function(){
+    
+}
