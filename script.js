00b2cedf9b3e86333761190cbe659e51f251e55c
SahithiBukka <93440705+SahithiBukka@users.noreply.github.com>
2023-06-05 21:51:04 +0530

    changes made

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var a={image:"Pic.jpeg",name:"Sahithi B.G",designation:"Student"};
-var b={image:"Pic1.jpeg",name:"Santhu",designation:"Software Developer"};
+var a={image:"./images/Pic.jpeg",name:"Sahithi B.G",designation:"Student"};
+var b={image:"./images/Pic1.jpeg",name:"Santhu",designation:"Software Developer"};
 var isa=true;
 var displaydata;
 var changeMypic=function(){
